b9afd5287de590950aac75902d86bd605a94eeae
Galen Sho Sato <galen.sato@gmail.com>
2023-03-01 21:56:30 -0500

    Completed Lesson

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "M", "Black", "Cuddly", True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 10, "F", "Orange", "Mean", True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 16, "F", "Chestnut", "Decisive", False);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 2, "M", "Black", "Rambuncious", True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 6, "F", "Brown", "Morose", True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 1, "M", "Green", "Heinous", True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 20, "F", "Black", "Matronly", False);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 3, "F", "White", "May Be Human", True);
+
